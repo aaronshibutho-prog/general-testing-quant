f7e108bb579afb76d222db2e4c31e8f83f32a5ec
unknown <aaronshibutho@gmail.com>
2026-07-08 21:39:11 +0530

    Understanding

--- moving avg.py
+++ moving avg.py
@@ -1,8 +1,8 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
 import pandas as pd
-TICKER = input('Enter the Ticket for 20 & 50 day MA: ')
-df = yf.download (TICKER, start= '1990-01-01', end= '2026-01-01')
+TICKER = "SPY"
+df = yf.download (TICKER, start= '2010-01-01', end= '2026-01-01')
 df['MA50'] = df['Close'].rolling(50).mean()
 df['MA20'] = df['Close'].rolling(20).mean()
 df = df.iloc[-200: , :]
@@ -11,4 +11,5 @@ plt.plot(df['MA50'], label ='50 Day MA')
 plt.plot(df['MA20'], label = '20 Day MA')
 plt.title(f'{TICKER} Comparing the 20 & 50 Day MA with the Closing Price')
 plt.legend(loc="lower right")
-plt.show()
+df.columns = df.columns.get_level_values(0)
+print(df)
